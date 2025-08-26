e6bf0d9e79a9f203b97e45c3f9d649bc7c55ef98
Haileab <womahaileab1428@gmail.com>
2025-08-26 22:44:27 +0300

    added way of sorting and removing dup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import Tree from "./Tree.js";
-const arr = [1, 2, 3, 4, 5, 6, 7];
+const arr = [1, 2, 3, 4, 5, 6, 7, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const modfarr = [...new Set(arr)].sort();
+console.log(modfarr);
 const tree = new Tree(arr);
-console.log(tree.root);
