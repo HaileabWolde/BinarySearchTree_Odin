47673116bf6cb3c10ddb49817aa837a30920dcdb
Haileab <womahaileab1428@gmail.com>
2025-08-27 13:28:29 +0300

    finshied with the insert function

--- Tree.js
+++ Tree.js
@@ -1,7 +1,27 @@
+import Node from "./Node.js";
 import buildarray from "./buildarray.js";
 class Tree{
     constructor(arr){
        this.root = buildarray(arr);
     }
+     insertRec(node, value){
+       // If tree is empty or we've reached a null node, create new node
+        if (node === null) {
+            return new Node(value);
+        }
+
+        // Recurse left or right based on value
+        if (value < node.data) {
+            node.left = this.insertRec(node.left, value);
+        } else if (value > node.data) {
+            node.right = this.insertRec(node.right, value);
+        }
+        // If value equals node.value, skip (no duplicates) or handle as needed
+        return node;
+    }
+    insert(value){
+        this.root = this.insertRec(this.root, value);
+    }
+   
 }
 export default Tree;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Tree from "./Tree.js";
-const arr = [1, 2, 3, 4, 5, 6, 7, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-const modfarr = [...new Set(arr)].sort();
-console.log(modfarr);
-const tree = new Tree(arr);
+const arr = [1, 2, 3, 4];
+const modArr = [...new Set(arr)].sort();
+const tree = new Tree(modArr);
+tree.insert(5);
+console.log(tree.root.right.right);
